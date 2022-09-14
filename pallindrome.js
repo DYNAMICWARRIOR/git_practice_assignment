6a5e8ca605bfb2db3af42bc143f9d00196a130af
subhakanta panda <meunknown466@gmail.com>
2022-09-15 00:01:36 +0530

    i am edit this code

--- pallindrome.js
+++ pallindrome.js
@@ -1,7 +1,13 @@
 //create a palindrome project for me
-var i =0 ;
-if(i%2==0){
-console.log("yes")
-}else{
-    console.log("no")
+function checkPalindrome(N, str) {
+    let bag="";
+    for(let i=N-1; i>=0; i--){
+        bag=bag+str[i];
+    }
+    if(str==bag){
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
 }
